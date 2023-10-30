fdb452048a2fec714a436c59da431a48245963f6
AntonioNoguera <michael.noguerag@uanl.edu.mx>
2023-10-30 13:22:03 -0600

    primeros métodos añadidos

--- src/Classes/Telefono.java
+++ src/Classes/Telefono.java
@@ -31,8 +31,35 @@ public class Telefono extends Clientes{
         this.saldoTelefono = saldoTelefono;
     }
 
-    //Metodos propuestos
-    //Get lada.
 
-    //Tiempo de llamada
+
+    //Metodos propuestos
+    //Recargar Saldo
+
+    public void recargarSaldo(float saldo){
+        this.saldoTelefono+=saldo;
+    }
+
+    //Lamada Tiempo de llamada
+    public void realizarLlamada(int duracionSegundos) {
+        float costoPorMinuto = 0.05f;
+        float costoLlamada = costoPorMinuto * duracionSegundos;
+        if (saldoTelefono >= costoLlamada) {
+            saldoTelefono -= costoLlamada;
+            System.out.println("Llamada a exitosa. Nuevo saldo: " + saldoTelefono);
+        } else {
+            System.out.println("Saldo insuficiente para realizar la llamada.");
+        }
+    }
+
+    public void enviarMensaje(int longitudMensaje) {
+        float costoPorCaracter = 0.05f;
+        float costoMensaje = longitudMensaje * costoPorCaracter;
+        if (saldoTelefono >= costoMensaje) {
+            saldoTelefono -= saldoTelefono;
+            System.out.println("Llamada a exitosa. Nuevo saldo: " + saldoTelefono);
+        } else {
+            System.out.println("Saldo insuficiente para realizar la llamada.");
+        }
+    }
 }
